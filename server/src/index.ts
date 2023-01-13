0fda7e44eb8fb3413e94bb3c38146fe38395e307
Serey Roth <sereyratanakroth@gmail.com>
2023-01-13 11:19:06 -0800

    Set up express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,15 @@
 import AppDataSource from "./AppDataSource"
+import express from "express";
+
 
 const main = async () => {
     await AppDataSource.initialize();
+
+    const app = express();
+
+    app.listen(4040, () => {
+        console.log('Server is up at Port: 4040');
+    })
 }
 
 main();
