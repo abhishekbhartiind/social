6c45dbf1b60f0226d4b81287131cda0f0a2f403d
Serey Roth <sereyratanakroth@gmail.com>
2023-01-13 20:44:58 -0800

    Create DocumentNodes for graphql schema

---
+++ server/src/schemas/index.ts
@@ -0,0 +1,6 @@
+import postDefs from './post';
+import userDefs from './user';
+import queryDefs from './query';
+import mutationDefs from './mutation';
+
+export const typeDefs = [postDefs, userDefs, queryDefs, mutationDefs];
