e12532dbb3d185dc23a953706179fbf68dd05056
Serey Roth <sereyratanakroth@gmail.com>
2023-01-13 19:56:04 -0800

    Set up apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import AppDataSource from "./AppDataSource"
 import express from "express";
+import { json } from "body-parser";
+import cors from "cors";
+import { ApolloServer } from "@apollo/server";
 
 
 const main = async () => {
@@ -7,9 +10,29 @@ const main = async () => {
 
     const app = express();
 
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolver: 
+    });
+    
+    app.use('/graphql', 
+        json(),
+        cors<cors.CorsRequest>({
+            credentials: true,
+            origin: [
+                "https://studio.apollographql.com",
+            ]
+        }),
+        expressMiddleware(apolloServer)
+    );
+
     app.listen(4040, () => {
         console.log('Server is up at Port: 4040');
     })
 }
 
 main();
+
+function expressMiddleware(apolloServer: ApolloServer<import("@apollo/server").BaseContext>): import("express-serve-static-core").RequestHandler<{}, any, any, import("qs").ParsedQs, Record<string, any>> {
+    throw new Error("Function not implemented.");
+}
