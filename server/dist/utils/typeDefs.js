f2997f4eb706a1a5e7f732f8f4e9fd1d416cc82d
Serey Roth <sereyratanakroth@gmail.com>
2023-01-14 18:36:06 -0800

    Delete dist folder

--- server/dist/utils/typeDefs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDefs = void 0;
-const load_files_1 = require("@graphql-tools/load-files");
-const merge_1 = require("@graphql-tools/merge");
-const typeArrays = (0, load_files_1.loadFilesSync)("src/schemas");
-exports.typeDefs = (0, merge_1.mergeTypeDefs)(typeArrays);
-//# sourceMappingURL=typeDefs.js.map
