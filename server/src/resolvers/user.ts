52208488019d5deb95aeda4defa1c8ed4fdc289d
Serey Roth <sereyratanakroth@gmail.com>
2023-01-15 18:10:51 -0800

    Update user resolvers to include sessions

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,33 +1,55 @@
 import argon2 from "argon2";
+import { COOKIE_NAME } from "../constants";
 import { dataManager } from "../AppDataSource";
 import { UserEntity as User } from "../entities/user";
 import { Resolvers } from "../generated/graphql";
 
 export const UserResolvers: Resolvers = {
     Query: {
-        me(_, { id }) {
+        me(_, __, { req }) {
+            if (!req.session.userId) {
+                return null;
+            }
             return dataManager.findOneBy(User, 
-                { id: parseInt(id) });
+                { id: req.session.userId });
         }
     },
     Mutation: {
-        async login(_, { username, password }) {
+        async login(_, { username, password }, { req }) {
             const existingUser = await dataManager.findOneBy(User, 
                 { username });
             if (!existingUser) {
-                return null;
+                return {
+                    errors: [{
+                        field: "username",
+                        message: "No user with this username!"
+                    }]
+                }
             }
             const validPassword = await argon2.verify(existingUser.password, password);
             if (!validPassword) {
-                return null;
+                return { 
+                    errors: [{
+                        field: "password",
+                        message: "Invalid password!"
+                    }]
+                }
             }
-            return existingUser;
+            req.session.userId = existingUser.id;
+            return { user: existingUser };
         },
-        async register(_, { username, password, email }) {
+        async register(_, 
+            { options: { username, password, email } }, 
+            { req }) {
             const existingUser = await dataManager.findOneBy(User, 
                 { username });
             if (existingUser) {
-                return existingUser;
+                return {
+                    errors: [{
+                        field: "username",
+                        message: "This username is taken!"
+                    }]
+                }
             } 
             const hashedPassword = await argon2.hash(password);
             const newUser = dataManager.create(User, 
@@ -37,10 +59,21 @@ export const UserResolvers: Resolvers = {
                     email 
                 });
             await dataManager.save(newUser);
-            return newUser;
+            req.session.userId = newUser.id;
+            return { user: newUser };
         }, 
-        logout() {
-            return true;
+        logout(_, __, { req, res }) {
+            return new Promise((resolve: any) => {
+                req.session.destroy((err) => {
+                    if (err) {
+                        resolve(false);
+                        return false;
+                    } 
+                    res.clearCookie(COOKIE_NAME);
+                    resolve(true);
+                    return true;
+                })
+            })
         }
     }
 };
