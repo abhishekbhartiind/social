4c487b2f73afbe8d06031dc6b69298e40d0b1750
Serey Roth <sereyratanakroth@gmail.com>
2023-01-15 18:10:06 -0800

    Set up sessions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,49 @@
+import "reflect-metadata";
 import AppDataSource from "./AppDataSource"
 import express from "express";
 import { json } from "body-parser";
 import cors from "cors";
 import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs } from "./utils/typeDefs";
 import { PostResolvers } from "./resolvers/Post";
 import { UserResolvers } from "./resolvers/user";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+import session from "express-session";
+import Redis from "ioredis";
+import connectRedis,{ RedisStoreOptions }  from "connect-redis";
+import { COOKIE_NAME, __prod__ } from "./constants";
+import { MyContext } from "./types/context";
 
 const main = async () => {
     await AppDataSource.initialize();
 
     const app = express();
 
+    //set up sessions and cookies
+    const RedisStore = connectRedis(session);
+    const redis = new Redis();
+    const redisStoreOptions: RedisStoreOptions = {
+        client: redis,
+        disableTouch: true
+    };
+
+    app.use(session({
+        name: COOKIE_NAME,
+        store: new RedisStore(redisStoreOptions),
+        secret: "asfasfqweasfqaer",
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+            httpOnly: true,
+            secure: __prod__,
+            sameSite: "lax",
+        },
+        saveUninitialized: false,
+        resave: false,
+    }));
+
+    //set up apollo server
     const schema = makeExecutableSchema({
         typeDefs,
         resolvers: [PostResolvers, UserResolvers]
@@ -21,6 +51,11 @@ const main = async () => {
     
     const apolloServer = new ApolloServer({
         schema,
+        plugins: [
+            ApolloServerPluginLandingPageLocalDefault({
+                includeCookies: true,
+            })
+        ]
     });
 
     await apolloServer.start();
@@ -33,7 +68,13 @@ const main = async () => {
                 "https://studio.apollographql.com",
             ]
         }),
-        expressMiddleware(apolloServer)
+        expressMiddleware(apolloServer, {
+            context: async ({ req, res }): Promise<MyContext> => ({
+                req,
+                res,
+                redis,
+            })
+        })
     );
 
     app.listen(4040, () => {
