b6c4234e16b21f9cddfa5ff9ded6b6fd588be362
Serey Roth <sereyratanakroth@gmail.com>
2023-01-16 17:48:49 -0800

    Add posts and likes fields to User entity

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { PostEntity as Post } from "./post";
+import { Like } from './like';
 
 @Entity()
 export class UserEntity {
@@ -13,4 +15,10 @@ export class UserEntity {
 
     @Column()
     email: string;
+
+    @OneToMany(() => Post, (post) => post.creator)
+    posts: Post[]
+
+    @OneToMany(() => Like, (like) => like.user)
+    likes: Like[]
 }
