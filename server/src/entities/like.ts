5b1e737c021293525aaf4c8f61dc1ea55b9ed2f6
Serey Roth <sereyratanakroth@gmail.com>
2023-01-16 17:48:06 -0800

    Create like entity

---
+++ server/src/entities/like.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
+import { PostEntity as Post } from "./post";
+import { UserEntity as User } from "./user";
+
+@Entity()
+export class Like {
+    @Column()
+    like: boolean;
+
+    @PrimaryColumn()
+    userId: number;
+
+    @ManyToOne(() => User, (user) => user.likes)
+    user: User;
+
+    @PrimaryColumn()
+    postId: number;
+
+    @ManyToOne(() => Post, (post) => post.likes)
+    post: Post;
+}
