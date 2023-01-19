c542fb6262394e1e327b5aa6e0acefff9c2955a9
Serey Roth <sereyratanakroth@gmail.com>
2023-01-18 21:49:25 -0800

    Update paths in codegen config file

--- web/codegen.ts
+++ web/codegen.ts
@@ -4,10 +4,13 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "http://localhost:4040/graphql",
-  documents: "graphql/**/*.graphql",
+  documents: "src/graphql/**/*.graphql",
   generates: {
-    "gql/graphql.tsx": {
-      plugins: ["typescript", "typescript-operations", "typescript-react-apollo"]
+    "src/gql/graphql.tsx": {
+      plugins: ["typescript", "typescript-react-apollo", "typescript-operations"],
+      config: {
+        withHooks: true,
+      }
     }
   }
 };
