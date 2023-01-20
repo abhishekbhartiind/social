706c5dd487f201bef6a7be1d6b632cbd20c767b5
Serey Roth <sereyratanakroth@gmail.com>
2023-01-19 21:33:22 -0800

    Add fieldPolicy to cache in withApollo client function to handle cache update after loading more posts

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -1,13 +1,37 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { withApollo } from "next-apollo";
 import { NextPageContext } from "next";
+import { PaginatedPosts } from "../gql/graphql";
 
 const client = (ctx: NextPageContext) => new ApolloClient({
     uri: "http://localhost:4040/graphql",
     headers: {
         cookie: (typeof window === 'undefined' ? ctx.req?.headers.cookie : undefined) || '',
     },
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+        typePolicies: {
+            Query: {
+                fields: {
+                    posts: {
+                        keyArgs: false,
+                        merge(
+                            existing: PaginatedPosts | undefined, 
+                            incoming: PaginatedPosts | undefined
+                        ): PaginatedPosts {
+                            return {
+                                ...incoming,
+                                hasMore: !!incoming?.hasMore,
+                                posts: [
+                                    ...(existing?.posts || []), 
+                                    ...(incoming?.posts || [])
+                                ],
+                            };
+                        }
+                    }
+                }
+            }
+        }
+    }),
     credentials: "include",
 });
 
