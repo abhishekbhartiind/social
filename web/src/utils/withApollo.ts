6bd41b0127b30559ab0e1e018531e6a556d5e5a1
Serey Roth <sereyratanakroth@gmail.com>
2023-01-19 19:34:12 -0800

    Add headers to withApollo client function

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -2,8 +2,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { withApollo } from "next-apollo";
 import { NextPageContext } from "next";
 
-const client = (ctx?: NextPageContext) => new ApolloClient({
+const client = (ctx: NextPageContext) => new ApolloClient({
     uri: "http://localhost:4040/graphql",
+    headers: {
+        cookie: (typeof window === 'undefined' ? ctx.req?.headers.cookie : undefined) || '',
+    },
     cache: new InMemoryCache(),
     credentials: "include",
 });
