d12138465baaa9026bc07eb117f0251248189e91
Serey Roth <sereyratanakroth@gmail.com>
2023-01-19 19:35:24 -0800

    Create home page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,32 @@
+import { Button, Flex, Heading, Stack } from "@chakra-ui/react";
+import NextLink from "next/link";
 import Layout from "../components/Layout";
+import { PostFeature } from "../components/PostFeature";
+import { useMeQuery, usePostsQuery } from "../gql/graphql";
 import withApollo from "../utils/withApollo";
 
 const Index = () => {
+    const { data: meData } = useMeQuery();
+    const { data, loading } = usePostsQuery();
+
     return (
         <Layout home>
-            <div>Home</div>
+            <Flex alignItems="center">
+                <Heading>Instapets</Heading>
+                <Button as={NextLink} href="/create-post" ml="auto">
+                    Create Post
+                </Button>
+            </Flex>
+            {loading && <div>Loading posts...</div>}
+            <br />
+            <Stack direction='column' spacing={4}>
+                {data?.posts.map(post => (
+                    <PostFeature 
+                        key={post.id} 
+                        post={post} 
+                        meId={meData?.me?.id} />
+                ))}
+            </Stack>
         </Layout>
     );
 }
