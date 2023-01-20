462bbdf1e3ab70c188789b37934b4ca6819c603c
Serey Roth <sereyratanakroth@gmail.com>
2023-01-19 21:33:39 -0800

    Add load more button to home page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -7,8 +7,23 @@ import withApollo from "../utils/withApollo";
 
 const Index = () => {
     const { data: meData } = useMeQuery();
-    const { data, loading } = usePostsQuery();
+    const { data, loading, fetchMore } = usePostsQuery({
+        variables: {
+            limit: 10,
+            cursor: null,
+        },
+        notifyOnNetworkStatusChange: true,
+    });
 
+    const handleLoadMore = () => {
+        if (data) {
+            fetchMore({
+                variables: {
+                    cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+                }
+            });
+        }
+    }
     return (
         <Layout home>
             <Flex alignItems="center">
@@ -19,14 +34,25 @@ const Index = () => {
             </Flex>
             {loading && <div>Loading posts...</div>}
             <br />
-            <Stack direction='column' spacing={4}>
-                {data?.posts.map(post => (
+            <Stack direction='column' spacing={4} mb={4}>
+                {data?.posts.posts.map(post => (
                     <PostFeature 
                         key={post.id} 
                         post={post} 
                         meId={meData?.me?.id} />
                 ))}
             </Stack>
+            {data && data.posts.hasMore && (
+                <Flex>
+                    <Button 
+                        isLoading={loading} 
+                        m="auto" 
+                        my={8} 
+                        onClick={handleLoadMore}>
+                            load more
+                    </Button>
+                </Flex>
+            )}
         </Layout>
     );
 }
