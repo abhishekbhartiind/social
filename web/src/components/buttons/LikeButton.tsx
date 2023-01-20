ba700fb9a5d007426493741019fc5b284e456338
Serey Roth <sereyratanakroth@gmail.com>
2023-01-19 22:15:00 -0800

    Change updateCache function to update likeCount

--- web/src/components/buttons/LikeButton.tsx
+++ web/src/components/buttons/LikeButton.tsx
@@ -14,16 +14,34 @@ const updateCacheAfterLike = (
     id: string,
     cache: ApolloCache<LikePostMutation>
 ) => {
-    cache.updateFragment<{ isLiked: boolean | null }>({
+    cache.updateFragment<{ 
+        isLiked: boolean | null,
+        likeCount: number, 
+    }>({
         id: "Post:" + id,
         fragment: gql`
             fragment _ on Post {
                 isLiked
+                likeCount
             }
         `
     }, 
     (data) => {
-        return { ...data, isLiked: !data?.isLiked };
+        if (data) {
+            const newIsLiked = !data.isLiked;
+            let newLikeCount = data.likeCount;
+            if (data.isLiked) {
+                newLikeCount -= 1;
+            } else {
+                newLikeCount += 1;
+            }
+            return { 
+                ...data, 
+                isLiked: newIsLiked,
+                likeCount: newLikeCount,
+            };
+        }
+        return data;
     });
 }
 
