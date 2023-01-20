3541bba7bfc02890eeda189735d79a1013997be7
Serey Roth <sereyratanakroth@gmail.com>
2023-01-19 19:34:56 -0800

    Create edit page for post

---
+++ web/src/pages/post/edit/[id].tsx
@@ -0,0 +1,94 @@
+import { Box, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import React from 'react'
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { usePostQuery, useUpdatePostMutation } from '../../../gql/graphql';
+import InputField from '../../../components/InputField';
+import Layout from '../../../components/Layout';
+import withApollo from '../../../utils/withApollo';
+
+type FormFields = {
+    text: string;
+    title: string;
+}
+
+const EditPost: React.FC<{}> = ({}) => {
+    const router = useRouter();
+    const postId = typeof router.query.id === 'string' ?
+        router.query.id : "";
+    const { data, loading, error } = usePostQuery({
+        skip: postId === "",
+        variables: { 
+            postId,
+        }
+    });
+
+    const methods = useForm<FormFields>({
+        values: {
+            title: data?.post?.title || "",
+            text: data?.post?.text || "",
+        }
+    });
+    const [updatePost] = useUpdatePostMutation();
+
+    const onSubmit: SubmitHandler<FormFields> = async (
+        values
+    ) => {
+        const { errors } = await updatePost({ 
+            variables: {
+                postId: data?.post?.id || "-1",
+                ...values
+            }
+        });
+        if (!errors) {
+            router.back();
+        }
+    }
+
+    if (loading) {
+        return <Layout>
+            <div>loading...</div>
+        </Layout>
+    }
+
+    if (error) {
+        return <div>{error.message}</div>
+    }
+
+    if (!data?.post) {
+        return <Layout>
+            <div>cannot find post</div>
+        </Layout>
+    }
+
+    return (
+        <Layout variant='small' home={false}>
+            <FormProvider {...methods}>
+                <form onSubmit={methods.handleSubmit(onSubmit)}>
+                    <InputField
+                        label='Title'
+                        name='title'
+                        placeholder='Enter title'
+                        type='text' />
+                    <Box mt={2}>
+                        <InputField
+                            label='Content'
+                            name='text'
+                            placeholder='Enter content'
+                            textArea />
+                    </Box>
+                    <Button 
+                        mt={4}
+                        isLoading={methods.formState.isSubmitting}
+                        colorScheme='teal'
+                        type='submit'
+                    >
+                        Done
+                    </Button>
+                </form>
+            </FormProvider>
+        </Layout>
+    );
+}
+
+export default withApollo({ ssr: false })(EditPost);
