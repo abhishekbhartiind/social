4b5617ac7b9de7cf9aa25f1d774a6888ee3b33a0
Serey Roth <sereyratanakroth@gmail.com>
2023-01-21 14:10:46 -0800

    Add pagination feature to CommentSection

--- web/src/components/post/CommentSection.tsx
+++ web/src/components/post/CommentSection.tsx
@@ -1,8 +1,9 @@
-import { Box, Stack } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Stack } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useBaseCommentsQuery } from "../../gql/graphql";
 import { AddComment } from "../AddComment";
 import { Comment } from "../comments/Comment";
+import { CommentWithReplies } from "../comments/CommentWithReplies";
 
 interface CommentSectionProps {
     postId: string;
@@ -11,35 +12,65 @@ interface CommentSectionProps {
 export const CommentSection: React.FC<CommentSectionProps> = ({
     postId
 }) => {
+
     const [parentComment, setParentComment] = useState<{
         author: string,
         id: string,
     } | null>(null);
 
-    const { data: commentData } = useBaseCommentsQuery({
+    const { data: commentData, loading, fetchMore } = useBaseCommentsQuery({
         skip: postId === "",
         variables: {
             postId,
+            options: {
+                limit: 5,
+            }
         },
+        notifyOnNetworkStatusChange: true,
     })
 
     const handleReplyClick = (author: string, id: string) => {
         setParentComment({ author, id });
     }
 
+    const handleLoadMore = () => {
+        if (commentData) {
+            const comments = commentData.baseComments.data;
+            const length = comments.length;
+            fetchMore({
+                variables: {
+                    options: {
+                        limit: 5,
+                        cursor: comments[length - 1].createdAt,
+                    }
+                }
+            });
+        }
+    }
+
     return (
-        <>
-            <Stack my={4} px={1}>
-                {commentData?.baseComments.map((comment) => (
-                    <Comment 
-                    key={comment.id} 
-                    comment={comment}
-                    onReply={() => handleReplyClick(
-                        comment.author.username, 
-                        comment.id
-                    )} />
-                ))}
+        <Box>
+            <Stack mt={4} px={1}>
+                {commentData?.baseComments.data.map((comment) => {
+                    const Component = comment.repliesCount > 0 ? CommentWithReplies : Comment;
+                    return <Component 
+                            comment={comment} 
+                            onReply={() => handleReplyClick(comment.author.username, comment.id)} />
+                })}
             </Stack>
+            {commentData && commentData.baseComments.hasMore && (
+                <Flex alignItems='center'>
+                    <Divider borderColor='black' maxW='8' />
+                    <Button
+                    variant='unstyled'
+                    ml={2}
+                    type='button'
+                    isLoading={loading}
+                    onClick={handleLoadMore}>
+                        View more comments
+                    </Button>
+                </Flex>
+            )}
             <Box
                 position="sticky"
                 bgColor="white"
@@ -55,6 +86,6 @@ export const CommentSection: React.FC<CommentSectionProps> = ({
                     parentCommentId={parentComment?.id} 
                     onSuccess={() => setParentComment(null)}/>
             </Box>
-        </>
+        </Box>
     );
 };
