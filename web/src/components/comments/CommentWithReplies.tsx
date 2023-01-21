40f5aa5e122d3f4287d35d82ee5c956ec1a0d07d
Serey Roth <sereyratanakroth@gmail.com>
2023-01-21 14:11:21 -0800

    Integrate comment replies feature into CommentWithReplies component

---
+++ web/src/components/comments/CommentWithReplies.tsx
@@ -0,0 +1,80 @@
+import { Box, Button, Divider, Flex, Stack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { BaseComment, useRepliesQuery } from '../../gql/graphql';
+import { Comment } from './Comment';
+
+interface CommentWithRepliesProps {
+  comment: BaseComment;
+  onReply: () => void;
+}
+
+export const CommentWithReplies: React.FC<CommentWithRepliesProps> = ({ 
+    comment, onReply 
+}) => {
+    const [repliesVisibility, setRepliesVisibility] = useState<'hide' | 'show'>(
+        'hide'
+    );
+
+    const { data, fetchMore } = useRepliesQuery({
+        skip: repliesVisibility === 'hide',
+        variables: {
+            parentCommentId: comment.id,
+            options: {
+                limit: 2
+            }
+        },
+    })
+
+    const handleLoadMore = () => {
+        if (data) {
+            const replies = data.replies.data;
+            const length = replies.length;
+            fetchMore({
+                variables: {
+                    options: {
+                        limit: 2,
+                        cursor: replies[length - 1].createdAt,
+                    }
+                }
+            });
+        }
+    }
+
+    const handleViewReply = () => {
+        if (repliesVisibility === 'hide') {
+            setRepliesVisibility('show');
+        } else if (repliesVisibility === 'show' && data?.replies.hasMore) {
+            handleLoadMore();
+        } else { 
+            setRepliesVisibility('hide');
+        }
+    };
+
+    return (
+        <Box>
+            <Comment comment={comment} onReply={onReply} />
+            {repliesVisibility === 'show' && (
+                <Stack ml={8} mt={2}>
+                {data?.replies.data.map(reply => (
+                    <Comment key={reply.id} comment={reply} onReply={onReply}/>
+                ))}
+                </Stack>
+            )}
+            <Flex alignItems='center' color='gray.500'>
+                <Divider borderColor='gray' maxW='16' />
+                <Button
+                    variant='unstyled'
+                    ml={2}
+                    type='button'
+                    fontSize='sm'
+                    onClick={handleViewReply}
+                >
+                    {repliesVisibility === 'hide' || data?.replies.hasMore 
+                    ? `View ${comment.repliesCount - (data?.replies.data.length || 0)} more 
+                    ${comment.repliesCount - (data?.replies.data.length || 0) === 1 ? 'reply' : 'replies'}`
+                    : 'Hide replies'}
+                </Button>
+            </Flex>
+        </Box>
+    );
+};
