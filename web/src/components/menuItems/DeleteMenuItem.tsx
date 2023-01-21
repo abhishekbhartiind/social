89874f0911dbbfa591c009f986cb49868b408939
Serey Roth <sereyratanakroth@gmail.com>
2023-01-21 15:35:41 -0800

    Create menu item for deleting posts

---
+++ web/src/components/menuItems/DeleteMenuItem.tsx
@@ -0,0 +1,38 @@
+import { MenuItem, MenuItemProps } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { useDeletePostMutation } from '../../gql/graphql';
+
+type DeleteMenuItemProps = MenuItemProps & {
+    id: string;
+}
+
+export const DeleteMenuItem: React.FC<DeleteMenuItemProps> = ({ 
+    id, ...rest 
+}) => {
+    const router = useRouter();
+    const [deletePost] = useDeletePostMutation();
+
+    const handleDeletePost = async (id: string) => {
+        await deletePost({
+            variables: { postId: id },
+            update: (cache) => {
+                cache.evict({ id: 'Post:' + id })
+            }
+        });
+        router.push('/');
+    };
+    
+    return (
+        <>
+        <MenuItem 
+        {...rest}
+        icon={<RiDeleteBin5Fill size={18}/>}
+        onClick={(_) => handleDeletePost(id)}
+        >
+            Delete post
+        </MenuItem>
+        </>
+    )
+};
