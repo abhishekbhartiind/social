16b6a0c9082bef53dccb2e6e23a443abdfd29dbf
Serey Roth <sereyratanakroth@gmail.com>
2023-01-21 15:35:12 -0800

    Create menu for editing and deleting posts

---
+++ web/src/components/EditDeleteMenu.tsx
@@ -0,0 +1,28 @@
+import { IconButton, Menu, MenuButton, MenuList } from "@chakra-ui/react";
+import React from "react";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { DeleteMenuItem } from "./menuItems/DeleteMenuItem";
+import { EditMenuItem } from "./menuItems/EditMenuItem";
+
+interface EditDeleteMenuProps {
+    id: string;
+}
+
+export const EditDeleteMenu: React.FC<EditDeleteMenuProps> = ({
+    id
+}) => {
+    return (
+        <Menu isLazy>
+            <MenuButton
+                as={IconButton}
+                aria-label="Edit/Delete Options"
+                icon={<GiHamburgerMenu />}
+                variant="outline"
+            />
+            <MenuList>
+                <EditMenuItem id={id} />
+                <DeleteMenuItem id={id} />
+            </MenuList>
+        </Menu>
+    );
+};
