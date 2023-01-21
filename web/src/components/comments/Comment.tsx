ab4419b65fa6bfefd589a2c283a4b80aa8b52c39
Serey Roth <sereyratanakroth@gmail.com>
2023-01-21 14:11:44 -0800

    Remove comment replies feature from Comment component

--- web/src/components/comments/Comment.tsx
+++ web/src/components/comments/Comment.tsx
@@ -1,23 +1,16 @@
 import { Avatar, Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { formatDistanceToNow } from "date-fns";
-import React, { useState } from "react";
+import React from "react";
 import { BaseComment, Reply } from "../../gql/graphql";
-import { Replies } from "./Replies";
 
 interface CommentProps {
   comment: BaseComment | Reply;
   onReply: () => void;
 }
 
-function isBaseComment(comment: BaseComment | Reply): comment is BaseComment {
-    return (comment as BaseComment).repliesCount !== undefined;
-}
-
 export const Comment: React.FC<CommentProps> = ({ 
     comment, onReply
 }) => {
-    const [repliesVisibility, setRepliesVisibility] = useState<'hide' | 'show'>('hide');
-
     return (
         <Flex alignItems='start' gap={2}>
             <Avatar size='sm' name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
@@ -49,22 +42,6 @@ export const Comment: React.FC<CommentProps> = ({
                         Reply
                     </Button>
                 </Flex>
-                {isBaseComment(comment) && repliesVisibility === 'show' && (
-                    <Replies parentCommentId={comment.id} onReply={onReply}/>
-                )}
-                {isBaseComment(comment) && comment.repliesCount > 0 && (
-                    <Text 
-                        cursor='pointer'
-                        onClick={() => setRepliesVisibility(
-                            repliesVisibility === 'hide' ? 
-                            'show' : 
-                            'hide')
-                        }>
-                        {repliesVisibility === 'hide' ? `
-                            View ${comment.repliesCount} more replies
-                        ` : 'Hide replies'}
-                    </Text>
-                )}
             </Box>
         </Flex>
     );
