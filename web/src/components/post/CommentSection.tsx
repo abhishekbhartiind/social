04c57eeb5d06c48d31523619003b727ca742763c
Serey Roth <sereyratanakroth@gmail.com>
2023-01-24 22:35:49 -0800

    Use CommentContext in CommentSection

--- web/src/components/post/CommentSection.tsx
+++ web/src/components/post/CommentSection.tsx
@@ -1,9 +1,9 @@
 import { Box, Button, Divider, Flex, Skeleton, Stack } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React from "react";
+import { CommentContextProvider } from "../../context/CommentContext";
 import { useBaseCommentsQuery } from "../../gql/graphql";
 import { AddComment } from "../AddComment";
 import { Comment } from "../comments/Comment";
-import { CommentWithReplies } from "../comments/CommentWithReplies";
 
 interface CommentSectionProps {
     postId: string;
@@ -12,12 +12,6 @@ interface CommentSectionProps {
 export const CommentSection: React.FC<CommentSectionProps> = ({
     postId
 }) => {
-
-    const [parentComment, setParentComment] = useState<{
-        author: string,
-        id: string,
-    } | null>(null);
-
     const { data: commentData, loading, fetchMore } = useBaseCommentsQuery({
         skip: postId === "",
         variables: {
@@ -28,10 +22,6 @@ export const CommentSection: React.FC<CommentSectionProps> = ({
         },
         notifyOnNetworkStatusChange: true,
     })
-
-    const handleReplyClick = (author: string, id: string) => {
-        setParentComment({ author, id });
-    }
 
     const handleLoadMore = () => {
         if (commentData) {
@@ -49,58 +39,48 @@ export const CommentSection: React.FC<CommentSectionProps> = ({
     }
 
     return (
-        <Box>
-            {commentData?.baseComments?.data && 
-            <Stack px={1}>
-                {commentData.baseComments.data.map((comment) => {
-                    return comment.repliesCount > 0 ? 
-                    <CommentWithReplies 
-                        key={comment.id}
-                        comment={comment} 
-                        onReply={() => 
-                            handleReplyClick(comment.author.username, comment.id)} />
-                    : <Comment 
-                        key={comment.id}
+        <CommentContextProvider>
+            <Box>
+                {commentData?.baseComments?.data && 
+                <Stack px={1}>
+                    {commentData.baseComments.data.map(comment => (
+                        <Comment 
+                        key={comment.id} 
                         loading={loading}
-                        comment={comment} 
-                        onReply={() => 
-                            handleReplyClick(comment.author.username, comment.id)} />
-                })}
-            </Stack>}
-            {commentData && commentData.baseComments.hasMore && (
-                <Skeleton 
-                isLoaded={!loading}
-                height='8'>
-                    <Flex alignItems='center'>
-                        <Divider borderColor='black' maxW='8' />
-                        <Button
-                        variant='unstyled'
-                        ml={2}
-                        type='button'
-                        color='gray'
-                        fontSize='sm'
-                        isLoading={loading}
-                        onClick={handleLoadMore}>
-                            View more comments
-                        </Button>
-                    </Flex>
-                </Skeleton>
-            )}
-            <Box
-                position="sticky"
-                bgColor='white'
-                bottom={0}
-                right={0}
-                left={0}
-                px={1}
-                py={1}
-            >
-                <AddComment 
-                    postId={postId} 
-                    parentCommentAuthor={parentComment?.author}
-                    parentCommentId={parentComment?.id} 
-                    onSuccess={() => setParentComment(null)}/>
+                        comment={comment} />
+                    ))}
+                </Stack>}
+                {commentData && commentData.baseComments.hasMore && (
+                    <Skeleton 
+                    isLoaded={!loading}
+                    height='8'>
+                        <Flex alignItems='center'>
+                            <Divider borderColor='black' maxW='8' />
+                            <Button
+                            variant='unstyled'
+                            ml={2}
+                            type='button'
+                            color='gray'
+                            fontSize='sm'
+                            isLoading={loading}
+                            onClick={handleLoadMore}>
+                                View more comments
+                            </Button>
+                        </Flex>
+                    </Skeleton>
+                )}
+                <Box
+                    position="sticky"
+                    bgColor="white"
+                    bottom={0}
+                    right={0}
+                    left={0}
+                    px={1}
+                    py={1}
+                >
+                    <AddComment postId={postId} />
+                </Box>
             </Box>
-        </Box>
+        </CommentContextProvider>
     );
 };
