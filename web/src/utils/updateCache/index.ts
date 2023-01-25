e98aef9aff2eed4bb6666dd5f0849e69a1384bfc
Serey Roth <sereyratanakroth@gmail.com>
2023-01-24 22:28:53 -0800

    Create utility functions for updating cache

---
+++ web/src/utils/updateCache/index.ts
@@ -0,0 +1,85 @@
+import { ApolloCache, gql } from "@apollo/client";
+
+export const updateCommentCountInCache = (
+    cache: ApolloCache<any>,
+    postId: string,
+    change: 'UP' | 'DOWN',
+    changeValue: number
+) => {
+    cache.updateFragment<{ 
+        commentCount: number
+    }>({
+        id: "Post:" + postId,
+        fragment: gql`
+            fragment CommentCount on Post {
+                commentCount
+            }
+        `
+    }, 
+    (data) => {
+        if (data) {
+            return { 
+                ...data, 
+                commentCount: data.commentCount + (change === 'UP' ? 1 : -1) * changeValue,
+            };
+        }
+        return data;
+    });
+}
+
+export const updateRepliesCountInCache = (
+    cache: ApolloCache<any>,
+    parentCommentId: string,
+    change: 'UP' | 'DOWN',
+    changeValue: number,
+) => {
+    cache.updateFragment<{ repliesCount: number }>({
+        id: `BaseComment:${parentCommentId}`,
+        fragment: gql`
+            fragment RepliesCount on BaseComment {
+                repliesCount
+            }
+        `
+    }, 
+    (data) => {
+        if (data) {
+            return {
+                ...data,
+                repliesCount: data.repliesCount + 
+                (change === 'UP' ? 1 : -1) * changeValue,
+            }
+        }
+        return data;
+    })
+}
+
+export const updateCacheAfterDeleteComment = (
+    cache: ApolloCache<any>,
+    postId: string,
+    commentId: string,
+    commentType: "BaseComment" | "Reply",
+    parentCommentId?: string
+) => {
+    let comment: { repliesCount?: number } | null | undefined;
+    if (commentType === "BaseComment") {
+        comment = cache.readFragment({
+            id: `${commentType}:${commentId}`,
+            fragment: gql`
+                fragment RepliesCount on ${commentType} {
+                    repliesCount
+                }
+            `
+        });
+    }
+
+    const deleteCount = (comment?.repliesCount || 0) + 1;
+    updateCommentCountInCache(cache, postId, 'DOWN', deleteCount);
+
+    //update replies count of parent comment if deleted comment is a reply
+    if (commentType === 'Reply' && parentCommentId) {
+        updateRepliesCountInCache(cache, parentCommentId, 'DOWN', 1)
+    }   
+
+    cache.evict({ id: commentType + ":" + commentId });
+    cache.gc();
+}
