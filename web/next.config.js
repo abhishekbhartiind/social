f0ab33f4fa1d3506747f18aff41bcb0ebaa70d00
Serey Roth <sereyratanakroth@gmail.com>
2023-01-27 23:15:49 -0800

    Modularize 'react-icons' imports to reduce initial load time

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  modularizeImports: {
+    'react-icons': {
+      transform: 'react-icons/{{member}}'
+    }
+  }
 }
 
 module.exports = nextConfig
