e551c6fd0b3c647ddfbfcbabb396115d5f3bfb24
Serey Roth <sereyratanakroth@gmail.com>
2023-01-28 20:27:38 -0800

    Create data loader utility function for total post count

--- server/src/utils/loaders/createUserLoader.ts
+++ server/src/utils/loaders/createUserLoader.ts
@@ -1,7 +1,7 @@
 import DataLoader from "dataloader";
+import { In } from "typeorm";
 import { dataManager } from "../../AppDataSource";
 import { UserEntity as User } from "../../entities/user";
-import { In } from "typeorm";
 
 export default function createUserLoader() {
     return new DataLoader<number, User>(
@@ -14,4 +14,27 @@ export default function createUserLoader() {
             return userIds.map(uid => userIdToUser[uid]);
         }
     );
+}
+
+export const createUserPostCountLoader = () => {
+    return new DataLoader<number, number>(
+        async (userIds) => {
+            // group rows by creatorId and count all posts with the same creator id for total post count
+            const postCounts: Array<{
+                creatorId: number,
+                totalPosts: string,
+            }> = await dataManager
+            .query(`
+                SELECT p."creatorId", COUNT(p.id) AS "totalPosts"
+                FROM post_entity p
+                WHERE p."creatorId" = ANY($1)
+                GROUP BY p."creatorId"
+            `, [userIds]);
+            const totalPostsByUserId: Record<string, number> = {};
+            postCounts.forEach(post => {
+                totalPostsByUserId[post.creatorId] = parseInt(post.totalPosts);
+            })
+            return userIds.map(userId => totalPostsByUserId[userId]);
+        }
+    )
 }
