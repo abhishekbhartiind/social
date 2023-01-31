1ed242fc03b5141a71ff3ed71253229e3dd73056
Serey Roth <sereyratanakroth@gmail.com>
2023-01-30 17:47:02 -0800

    Add url for database to AppDataSource

--- server/src/AppDataSource.ts
+++ server/src/AppDataSource.ts
@@ -5,16 +5,20 @@ import { UserEntity as User } from "./entities/user";
 import { Like } from "./entities/like";
 import { CommentEntity as Comment } from "./entities/comment";
 import { CommentLike } from "./entities/comment-like";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    database: 'instapets',
-    username: 'serey',
-    password: 'instapets',
-    synchronize: true,
+    // database: 'instapets',
+    // username: 'serey',
+    // password: 'instapets',
+    url: process.env.DATABASE_URL,
+    //synchronize: true,
     logging: true,
     entities: [Post, User, Like, Comment, CommentLike],
-    migrations: [path.join(__dirname, '/migrations/**/*.ts')]
+    migrations: [path.join(__dirname, '/migrations/*')]
 });
 
 export const dataManager = AppDataSource.manager;
