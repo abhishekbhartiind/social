e69410addf2fa73340b6df7e141ab4fe89e92ae4
Serey Roth <sereyratanakroth@gmail.com>
2023-02-04 22:18:52 -0800

    Set up media stream based on activeDeviceId

--- web/src/utils/useCameraStream.ts
+++ web/src/utils/useCameraStream.ts
@@ -1,67 +1,82 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useCameraContext } from "../context/CameraContext";
+
+type CameraStreamStateType = 'close' | 'loading' | 'open' | 'failure';
 
 export const useCameraStream = () => {
-    const [cameraMode, setCameraMode] = useState<'user' | 'environment'>(
-        'user'
-    );
 
+    const [stream, setStream] = useState<MediaStream | null>(null);
     const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
     const [activeDeviceId, setActiveDeviceId] = useState<string | undefined>();
+    const [cameraStreamState, setCameraStreamState] = useState<CameraStreamStateType>(
+        'close'
+    );
 
-    const handleToggleCameraMode = () => {
-        setCameraMode(prevState => prevState === 'user' ? 
-            'environment' : 'user');
-    }
+    const { video } = useCameraContext();
 
     const handleSetActiveDeviceId = (id: string) => {
         setActiveDeviceId(id);
     }
 
-    const openCameraStream = (
-        onSuccess: (stream: MediaStream) => void, 
-        onFailure: (error: Error) => void
-    ) => {
-        const constraints = { 
-            video: { 
-                deviceId: activeDeviceId ? { exact: activeDeviceId } : undefined,
-                facingMode: cameraMode,
-                width: { min: 1024, ideal: 1280 },
-                height: { min: 576, ideal: 720 }
-            }, 
-            audio: false,
-        };
-
-        const handleSuccess = (stream: MediaStream) => {
-            navigator.mediaDevices
-                .enumerateDevices()
-                .then(devices => 
-                    setDevices(devices.filter(device => 
-                    device.kind == 'videoinput')));
-            onSuccess(stream);
-        }
-
-        return navigator.mediaDevices.getUserMedia(constraints)
-        .then(handleSuccess)
-        .catch(onFailure);
-    }
-
-    const closeCameraStream = (video?: HTMLVideoElement | null) => {
-        if (!video) return;
-        video.pause();
-        const stream = video.srcObject;
+    const closeCameraStream = () => {
+        if (!video.current) return;
+        video.current.pause();
+        const stream = video.current.srcObject;
         if (stream instanceof MediaStream) {
             stream.getTracks().forEach(track => {
                 track.stop();
             });
         }
-        video.srcObject = null;
+        video.current.srcObject = null;
     }
+
+    useEffect(() => {
+        (async () => {
+            setCameraStreamState('loading');
+
+            if (stream) {
+                stream.getTracks().forEach(track => {
+                  track.stop();
+                });
+            }
+            
+            const constraints = { 
+                video: { 
+                    deviceId: activeDeviceId ? 
+                        { exact: activeDeviceId } : undefined,
+                    facingMode: 'user',
+                    width: { min: 1024, ideal: 1280 },
+                    height: { min: 576, ideal: 720 }
+                }, 
+                audio: false,
+            };
+    
+            try {
+                const stream = await navigator.mediaDevices
+                    .getUserMedia(constraints);
+                const devices = await navigator.mediaDevices
+                    .enumerateDevices();
+
+                setDevices(devices.filter(device => device.kind == 'videoinput'));
+
+                setStream(stream);
+                if (video.current) {
+                    video.current.srcObject = stream;
+                    video.current.play();
+                    setCameraStreamState('open');
+                }
+            } catch (error) {
+                console.error(`An error occurred; ${error}`)
+                setCameraStreamState('failure');
+            }
+        })();
+    }, [activeDeviceId]);
 
     return {
         devices,
-        openCameraStream,
+        activeDeviceId,
+        cameraStreamState,
         closeCameraStream,
-        handleToggleCameraMode,
         handleSetActiveDeviceId,
     }
 }
