fd95ae9bde101816abb34f28ad9c848614272398
Serey Roth <sereyratanakroth@gmail.com>
2023-02-04 19:11:42 -0800

    Add CameraContextProvideer to app

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { CameraContextProvider } from '../context/CameraContext'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
-            <Component {...pageProps} />
+            <CameraContextProvider>
+                <Component {...pageProps} />
+            </CameraContextProvider>
         </ChakraProvider>
     )
 }
